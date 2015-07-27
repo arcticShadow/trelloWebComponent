f3bab37febea307a9559a83e280009cd4db7dddd
Cole Diffin <cole.diffin@orionhealth.com>
2015-07-27 16:25:10 +1200

    Why the fuck it not working

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,56 @@
 'use strict';
 var gulp = require('gulp'),
   clean = require('gulp-clean'),
-  header = require('gulp-header'),
   rename = require('gulp-rename'),
   uglify = require('gulp-uglify'),
   jade = require('gulp-jade'),
-  browserify = require('gulp-browserify'),
-  markdox = require('gulp-markdox'),
-  pkg = require('./package.json'),
-  template = require('lodash.template');
+  brfs = require('gulp-brfs'),
+  markdox = require('gulp-markdox');
+  // pkg = require('./package.json'),
+  // template = require('lodash.template');
 
 gulp.task('default', ['clean', 'compile']);
 gulp.task('compile', ['compile:lib', 'doc']);
-gulp.task('compile:lib', ['browserify:lib']);
 
 
 gulp.task('watch', function() {
-  gulp.watch(['lib/*','config.j*'], ['compile:lib']);
-  gulp.watch('dist/src/*.html', ['browserify']);
-  // gulp.watch('*.md',['doc']);
+  gulp.watch('lib/*', ['compile']);
 });
 
-gulp.task('watch:lib', function() {
-  gulp.watch('lib/*', ['compile:lib']);
-});
+gulp.task('clean', ['clean:js', 'clean:jade']);
+gulp.task('compile', ['compile:js', 'compile:jade']);
 
-gulp.task('clean', ['clean:browserify']);
-gulp.task('clean:browserify', ['clean:browserify:lib']);
 
-gulp.task('clean:browserify:lib', function() {
+gulp.task('clean:js', function() {
   return gulp.src(['dist/trelloWebComponent*'], { read: false })
     .pipe(clean());
 });
 
-gulp.task('clean:jade:lib', function() {
+gulp.task('clean:jade', function() {
   return gulp.src(['dist/src/*.html'], { read: false })
     .pipe(clean({force:true}));
 });
 
-gulp.task('browserify', ['browserify:lib']);
 
-gulp.task('browserify:lib', ['clean:browserify:lib', 'jade:lib'], function() {
+gulp.task('compile:js', ['clean:js', 'compile:jade'], function() {
   return gulp.src('lib/trelloWebComponent.js')
-    .pipe(browserify({ transform: ['brfs'], standalone: 'trelloWebComponent'  }))
-    // .pipe(header(template([
-    //   '/*!',
-    //   ' * <%= name %> v<%= version %>',
-    //   ' *',
-    //   ' * Copyright <%= new Date().getFullYear() %>, <%= author.name %>',
-    //   ' * This content is released under the <%= licenses[0].type %> license',
-    //   ' * <%= licenses[0].url %>',
-    //   ' */\n\n'
-    // ].join('\n'), pkg)))
-    .pipe(gulp.dest('dist'))
     .pipe(rename('trelloWebComponent.min.js'))
     .pipe(uglify())
-    // .pipe(header(template([
-    //   '/*! <%= name %> v<%= version %> ',
-    //   '© <%= new Date().getFullYear() %> <%= author.name %>, ',
-    //   '<%= licenses[0].type %> License */\n'
-    // ].join(''), pkg)))
+    .pipe(brfs())
     .pipe(gulp.dest('dist'));
 });
+gulp.task('brfs',function(){
+return gulp.src('lib/trelloWebComponent.js')
+    .pipe(brfs())
+    .pipe(gulp.dest('dist'));
 
-gulp.task('jade:lib', ['clean:jade:lib'], function() {
+});
+
+
+gulp.task('compile:jade', ['clean:jade'], function() {
   return gulp.src('lib/src/*.jade')
     .pipe(jade({ pretty: true }))
-    .pipe(gulp.dest('dist/src'))
-    ;
+    .pipe(gulp.dest('dist/src'));
 });
 
 gulp.task('clean:doc', function() {
